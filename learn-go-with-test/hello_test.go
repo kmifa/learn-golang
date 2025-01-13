443b1a68c2b86af827b5c7376444ed11f8f0683d
shinji asako <13231699+kmifa@users.noreply.github.com>
2025-01-14 07:15:09 +0900

    hello world リファクタリング

--- learn-go-with-test/hello_test.go
+++ learn-go-with-test/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper() // This tells the test suite that the helper function is just that - a helper function - and should be skipped in the test output.
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
 }
