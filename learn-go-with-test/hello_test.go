7c2ac3011a3d396a99687670d3d8c0bc90c79558
shinji asako <13231699+kmifa@users.noreply.github.com>
2025-01-14 09:04:50 +0900

    hello world リファクタリング

--- learn-go-with-test/hello_test.go
+++ learn-go-with-test/hello_test.go
@@ -10,17 +10,31 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		if got != want {
 			assertCorrectMessage(t, got, want)
 		}
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Bob", "French")
+		want := "Bonjour, Bob"
+
+		assertCorrectMessage(t, got, want)
 	})
 }
