c86d05c6f40f5bc2ffaf032bc314abf683981597
shinji asako <13231699+kmifa@users.noreply.github.com>
2025-01-17 10:28:59 +0900

    context with test

--- learn-go-with-test/context/context.go
+++ learn-go-with-test/context/context.go
@@ -1,16 +1,23 @@
 package context1
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return
+		}
+		fmt.Fprint(w, data)
+
 	}
 }
