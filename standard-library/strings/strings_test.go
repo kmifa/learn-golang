0cdf9d53974c79bdb672e6c7d84f23616511230a
shinji asako <13231699+kmifa@users.noreply.github.com>
2025-01-22 13:54:59 +0900

    add Fields, HasPrefix test

--- standard-library/strings/strings_test.go
+++ standard-library/strings/strings_test.go
@@ -1,6 +1,7 @@
 package strings
 
 import (
+	"slices"
 	"strings"
 	"testing"
 )
@@ -10,27 +11,85 @@ func TestTrimPrefix(t *testing.T) {
 		got := strings.TrimPrefix("Hello, world!", "Hello, ")
 		want := "world!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertGotWant(t, got, want)
 	})
 
 	t.Run("empty prefix", func(t *testing.T) {
 		got := strings.TrimPrefix("Hello, world!", "")
 		want := "Hello, world!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertGotWant(t, got, want)
 	})
 
 	t.Run("prefix space", func(t *testing.T) {
 		got := strings.TrimPrefix("  Hello, world!", "Hello")
 		want := "  Hello, world!"
 
-		if got != want {
+		assertGotWant(t, got, want)
+	})
+}
+
+func TestFields(t *testing.T) {
+	t.Run("fields found", func(t *testing.T) {
+		got := strings.Fields("Hello, world!")
+		want := []string{"Hello,", "world!"}
+
+		if !slices.Equal(got, want) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
 
+	t.Run("fields found spaces ", func(t *testing.T) {
+		got := strings.Fields("Hello,  world!   Taro     ")
+		want := []string{"Hello,", "world!", "Taro"}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("empty string", func(t *testing.T) {
+		got := strings.Fields("")
+		want := []string{}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func TestHasPrefix(t *testing.T) {
+	t.Run("prefix found", func(t *testing.T) {
+		got := strings.HasPrefix("Hello, world!", "Hello")
+		want := true
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+
+	t.Run("prefix not found", func(t *testing.T) {
+		got := strings.HasPrefix("Hello, world!", "world")
+		want := false
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+
+	t.Run("empty prefix", func(t *testing.T) {
+		got := strings.HasPrefix("Hello, world!", "")
+		want := true
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+}
+
+func assertGotWant(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
